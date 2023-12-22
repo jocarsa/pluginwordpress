3fd5d0477aa1a617e84f3733d8b70306f3caccf9
jocarsa <info@josevicentecarratala.com>
2023-12-22 12:05:51 +0100

    declaracion

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/*
+    Plugin Name: ConectarAPI
+    Description: Plugin para conectarse a una API externa
+    Version: 1.0.0
+    Author: Jose Vicente Carratala
+    Author URI: https://jocarsa.com
+*/
+?>
