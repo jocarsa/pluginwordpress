8d7627b2641ed80ab3a854fe9ba65af4854d57a2
jocarsa <info@josevicentecarratala.com>
2023-12-22 12:10:42 +0100

    funcion

--- index.php
+++ index.php
@@ -7,19 +7,21 @@
     Author URI: https://jocarsa.com
 */
 
+function dameClientes(){
+    $api_url = 'http://127.0.0.1:8080/clientes';
+    $ch = curl_init($api_url);
 
-$api_url = 'http://127.0.0.1:8080/clientes';
-$ch = curl_init($api_url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+    $response = curl_exec($ch);
 
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
-$response = curl_exec($ch);
+    if (curl_errno($ch)) {
+        echo 'cURL Error: ' . curl_error($ch);
+    } else {
+        echo $response;
+    }
 
-if (curl_errno($ch)) {
-    echo 'cURL Error: ' . curl_error($ch);
-} else {
-    echo $response;
+    curl_close($ch);
 }
 
-curl_close($ch);
 
 ?>
