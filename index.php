7568b865a67a50eec3f77775d8238bcbd7b54100
jocarsa <info@josevicentecarratala.com>
2023-12-22 12:26:22 +0100

    varias api

--- index.php
+++ index.php
@@ -7,36 +7,69 @@
     Author URI: https://jocarsa.com
 */
 
-function dameClientes(){
-    $api_url = 'http://127.0.0.1:8080/clientes';
-    $ch = curl_init($api_url);
+function dameAPI($atts){
+    
+    // Define default values for the shortcode attributes
+    $atts = shortcode_atts(
+        array(
+            'api' => 'clientes', 
+            'api' => 'productos', 
+        ),
+        $atts,
+        'dame_api'
+    );
 
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
-    $response = curl_exec($ch);
-
-    if (curl_errno($ch)) {
-        echo 'cURL Error: ' . curl_error($ch);
-    } else {
-
-        $data = json_decode($response);
-
-        if (is_array($data)) {
-            echo '<div class="contenido">';
-            
-            foreach ($data as $item) {
-                echo '<article class="entrada">';
-                echo '<h2>' . esc_html($item->nombre) . '</h2>';
-                echo '<p>' . esc_html($item->descripcion) . '</p>';
-                echo '</article>';
+    $api = $atts['api'];
+    
+    switch($api){
+        case "clientes":
+            $api_url = 'http://127.0.0.1:8080/clientes';
+            $ch = curl_init($api_url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+            $response = curl_exec($ch);
+            if (curl_errno($ch)) {
+                echo 'cURL Error: ' . curl_error($ch);
+            } else {
+                $data = json_decode($response);
+                if (is_array($data)) {
+                    echo '<div class="contenido">';         
+                    foreach ($data as $item) {
+                        echo '<article class="entrada">';
+                        echo '<h2>' . esc_html($item->nombre) . '</h2>';
+                        echo '<p>' . esc_html($item->descripcion) . '</p>';
+                        echo '</article>';
+                    }
+                    echo '</div>';
+                }
             }
-
-            echo '</div>';
-        }
+            curl_close($ch);
+            break;
+        case "productos":
+            $api_url = 'http://127.0.0.1:8080/productos';
+            $ch = curl_init($api_url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+            $response = curl_exec($ch);
+            if (curl_errno($ch)) {
+                echo 'cURL Error: ' . curl_error($ch);
+            } else {
+                $data = json_decode($response);
+                if (is_array($data)) {
+                    echo '<div class="contenido">';         
+                    foreach ($data as $item) {
+                        echo '<article class="entrada">';
+                        echo '<h2>' . esc_html($item->nombre) . '</h2>';
+                        echo '<p>' . esc_html($item->descripcion) . '</p>';
+                        echo '</article>';
+                    }
+                    echo '</div>';
+                }
+            }
+            curl_close($ch);
+            break;
     }
-
-    curl_close($ch);
+    
 }
 
-add_shortcode("dame_clientes","dameClientes");
+add_shortcode("dame_api","dameAPI");
 
 ?>
--- servidor.py
+++ servidor.py
@@ -12,6 +12,10 @@ def home():
 def clientes():
     return '[{"nombre":"Jose Vicente","descripcion":"descripcion del primer cliente"},{"nombre":"Juan","descripcion":"descripcion del segundo cliente"}]'
 
+@app.route('/productos')
+def productos():
+    return '[{"nombre":"Producto 1","descripcion":"descripcion del producto 1"},{"nombre":"Producto 2","descripcion":"descripcion del segundo producto"}]'
+
 
 if __name__ == '__main__':
     app.run(port=8080)
